132cfee0df5d8bfdda9a45ffb60e3e9f991160be
mkafonso <mkafonso.dev@gmail.com>
2026-05-13 04:48:40 -0300

    feat: update vitest configuration to include global setup and increase test timeout

--- __tests__/vitest.config.ts
+++ __tests__/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['unit/**/*.spec.ts', 'integration/**/*.test.ts'],
+    globalSetup: ['integration/setup/global.ts'],
     environment: 'node',
+    testTimeout: 30_000,
   },
 })
